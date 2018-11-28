0387be3f80280721a7569319823968870425f081
luis-alarcon <alarcon.services@protonmail.com>
2018-11-28 11:56:35 +0000

    Form template almost done

--- app.js
+++ app.js
@@ -8,6 +8,12 @@ const app = express();
 // Static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Signup Route
+app.post('/signup', (req, res) => {
+  //
+  const { pregunta_1, pregunta_2, pregunta_3, pregunta_4, pregunta_5, pregunta_6, pregunta_7, pregunta_8, firstName, lastName, email } = req.body;
+};
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`Server started on ${PORT}`));
